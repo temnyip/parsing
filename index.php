3e43f2f98c34122c9ddf0e4d2f0d86105f26d34e
Hotaro <hotaro@yandex.ru>
2019-10-25 00:16:43 +0300

    PARSER SQL

--- index.php
+++ index.php
@@ -1,65 +1,126 @@
 <?php
 //echo phpinfo();
 //exit;
-use simplehtmldom_1_5\simple_html_dom;
-use function simplehtmldom_1_5\str_get_html;
-
 require_once 'vendor/autoload.php';
+$img_url = $_POST['img'];
+$title = $_POST['name'];
+$text = $_POST['text'];
+$end_date = $_POST['date'];
+
+$dotenv = Dotenv\Dotenv::create(__DIR__, 'prod.env');
+$dotenv->load();
+
 $db = new Db(
-    "127.0.0.1",
-    "3306",
-    "first",
-    "root",
-    ""
+    getenv('DBHost'),
+    getenv('DBPort'),
+    getenv('DBName'),
+    getenv('DBUser'),
+    getenv('DBPassword')
 );
+
+//$db = new Db(
+//    "127.0.0.1",
+//    "3306",
+//    "first",
+//    "root",
+//    ""
+//);
 require_once 'phpQuery/phpQuery/phpQuery.php';
-require_once 'curl/curl.php';
-require_once 'Simple/simple/simple_html_dom.php';
-header('Content-type: text/html; charset=utf-8');
+//header('Content-type: text/html; charset=utf-8');
 
 function print_arr($arr) {
     echo '<pre>' . print_r($arr, true) . '</pre>';
 }
-function get_content($curl) {
-    $ch = curl_init($curl);
+
+function get_content($url) {
+    $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $res = curl_exec($ch);
     curl_close($ch);
     return $res;
+
 }
 
-$curl = new Curl();
-$query = '';
-$response = $curl->get('https://www.coupons.com/store-loyalty-card-coupons/' . $query);
-//С помощью DOM выкачиваем код
-//var_dump($response->body);
-$dom = get_content($response);
-$doc= phpQuery::newDocument($response->body);
-//////////////////////////////////////////////////////////////////////////////
+function parser($url, $start, $end) {
+    if($start < $end) {
+        // $file = file_get_contents($url);
+        $file = get_content($url);
+        $doc = phpQuery::newDocument($file);
+        $stores = [];
 
-/////////////////////////////////////////////////////////////////////////////
-$products = $doc->find('.store-pod');
-foreach($products as $product) {
-    $pq = pq($products);
-    $pq ->find('.store-cp-savings')->remove();
-    $text = $pq->find('.store-browse')->html();
-    $url = $pq->attr('href');
+        foreach ($doc->find('.page .media') as $article) {
+            $store = [];
+
+            $article = pq($article);
+            $img = $article->find('.media-object img')->attr('src'); //выводит лого
+            $name = $article->find('.pod_brand')->html();    //вывод название магазина + купоны и бонусы (с помощью remove удаляем лишнее)
+            $description = $article->find('.pod_description')->html();
+            $date = $article->find('.pod_expiry')->html();
+            $store['img'] = $img;
+            $store['name'] = htmlspecialchars($name);
+            $store['description'] = htmlspecialchars($description);
+            $partsDate = explode('Exp: ', $date);
+            $date = \DateTime::createFromFormat('m/d/y', $partsDate[1]);
+            $store['date'] = $date->format('Y-m-d');
+//            $store['date'] = date('Y-m-d', strtotime($date));
+            $stores[] = $store;
+
+
+//            echo "<img src='$img'>";
+//            echo '<hr>';
+//            echo $name;
+//            echo '<hr>';
+//            echo $description;
+//            echo '<hr>';
+//            echo $date;
+//            echo '<hr>';
+        }
+        return $stores;
     }
+}
+$url = 'https://www.coupons.com/store-loyalty-card-coupons/acme-coupons/';
+$start = 0;
+$end = 3;
+$stores = parser($url, $start, $end);
+function saveStores($stores) {
+    global $db;
+    foreach($stores as $store) {
+        $sql = "INSERT INTO `cupons` 
+ (`shop_id`, `img_url`, `title`, `text`, `end_date`) 
+ 
+                 VALUES (
+'1', '{$store['img']}', '{$store['name']}', '{$store['description']}', '{$store['date']}'
+                 );";
+       $db->query($sql);
+    }
+}
+saveStores($stores);
+//$ch = curl_init();
+//curl_setopt($ch, CURLOPT_URL, $url);
+////curl_setopt($ch, CURLOPT_HEADER, true);
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+////curl_setopt($ch, CURLOPT_NOBODY, true);
+//curl_exec($ch);
+//curl_close($ch);
 
-    echo $text;
-    echo '<hr>';
-    echo $url;
-    echo '<hr>';
+// весь блок для записи в файл
+//    $fp = fopen("file.txt", "w");
+//    $ch = curl_init($url);
+//    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+//    curl_setopt($ch, CURLOPT_FILE, $fp);
+//    $res = curl_exec($ch);
+//    curl_close($ch);
+//    var_dump($res);
 
-//    $urls = preg_match_all('|<a href="(.*?)">|uis', $response, $result);
-//    $urls = $pq->find('.other-stores .store-pod')->html('$urls');
-//    for($i=0; $i<$urls; $i++){
-//     echo $result[1][$i].'<hr>';
-//    }
+
+//$ch = curl_init($url);
+//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//$res = curl_exec($ch);
+//curl_close($ch);
+
 
 
 
 ?>
-
-
